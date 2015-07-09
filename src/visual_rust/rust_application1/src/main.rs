fd73af9140e00b627626d50bf853e65dc6980f2c
kushal <kushaldeveloper@gmail.com>
2015-07-09 13:53:05 -0500

    stop using using

--- src/visual_rust/rust_application1/src/main.rs
+++ src/visual_rust/rust_application1/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!")
-}
+fn main(){
+    println!("Guess the number");
+    println!("Please input your guess."); 
+    let mut guess = String::new();
+	std::io::stdin().read_line(&mut guess)
+    .ok() 
+    .expect("Failed to read line"); 
+    println!("You guessed: {}", guess);
+    }
