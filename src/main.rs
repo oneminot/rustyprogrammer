c9c98d2a900a73eec7440e8ef72b2249f0dc94f5
kushal <kushaldeveloper@gmail.com>
2015-09-06 13:34:38 -0400

    add formatted print example from http://rustbyexample.com/hello/print.html and look for warning for input_integer in travis-ci

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 fn main(){
 	let input_integer: i32 = 42;
-	println!("The value of input_integer is: {}", input_integer);
+	println!("{subject} {verb} {predicate}", 
+	predicate = "over the laxy dog",
+	subject = "the quick brown fox", 
+	verb = "jumps");
 }
