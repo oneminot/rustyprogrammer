2caee842ed39e05257fd735a6649c3c2948e02a2
kushal <kushaldeveloper@gmail.com>
2016-01-08 20:19:41 -0500

    add from dinging philosophers in 3.2 in rust book

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
-fn main(){
-	let input_integer: i32 = 42;
-	println!("{subject} {verb} {predicate}", 
-	predicate = "over the laxy dog",
-	subject = "the quick brown fox", 
-	verb = "jumps");
+struct Philosopher {
+    name: String,
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+        }
+    }
+
+	fn eat(&self){
+		println!("{} is done eating.", self.name);
+	}
+}
+
+fn main() {
+    let philosophers = vec![
+	Philosopher::new("Judith Butler"),
+    Philosopher::new("Gilles Deleuze"),
+    Philosopher::new("Karl Marx"),	
+	Philosopher::new("Emma Goldman"), 
+	Philosopher::new("Michel Foucault"),
+	];
+	for p in philosophers {
+		p.eat();
+	}
 }
