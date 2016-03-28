d0e21cf9bb9522c05649f5677e6b1795835cbeab
kushal <kushaldeveloper@gmail.com>
2016-03-27 23:37:47 -0400

    add a year of birth for the philosopher

--- src/visual_rust/rust_application1/src/main.rs
+++ src/visual_rust/rust_application1/src/main.rs
@@ -1,26 +1,28 @@
 struct Philosopher {
-    name: String,
+    name: String, 
+	year: i16,
 }
 
 impl Philosopher {
-    fn new(name: &str) -> Philosopher {
+    fn new(name: &str, year: i16) -> Philosopher {
         Philosopher {
             name: name.to_string(),
+			year: year as i16,
         }
     }
 
 	fn eat(&self){
-		println!("{} is done eating.", self.name);
+		println!("{} of {} is done eating.", self.name, self.year);
 	}
 }
 
 fn main() {
     let philosophers = vec![
-	Philosopher::new("Judith Butler"),
-    Philosopher::new("Gilles Deleuze"),
-    Philosopher::new("Karl Marx"),	
-	Philosopher::new("Emma Goldman"), 
-	Philosopher::new("Michel Foucault"),
+	Philosopher::new("Judith Butler", 1956),
+    Philosopher::new("Gilles Deleuze", 1925),
+    Philosopher::new("Karl Marx", 1818),	
+	Philosopher::new("Emma Goldman", 1869), 
+	Philosopher::new("Michel Foucault", 1926),
 	];
 	for p in philosophers {
 		p.eat();
