8572ec794023048576d4b97c61439265b259ceb3
kushal <kushaldeveloper@gmail.com>
2016-06-05 12:18:54 -0400

    use mem
    get size of val

--- src/visual_rust/rust_application1/src/main.rs
+++ src/visual_rust/rust_application1/src/main.rs
@@ -1,4 +1,11 @@
+use std::mem;
 fn main() {
     let a:u8 = 123;
     println!("the value of a is {}", a);
+    let mut b:i8 = 0;
+    println!("b = {}", b);
+    b = 42;
+    println!("b = {}", b);
+    let mut c = 123456789;
+    println!("c = {}, size = {} bytes", c, mem::size_of_val(&c));
 }
