2c6791d93f1b386c80c2bdeb0d015f86677d83d0
kushal <kushaldeveloper@gmail.com>
2016-06-05 12:44:24 -0400

    use exponent

--- src/visual_rust/rust_application1/src/main.rs
+++ src/visual_rust/rust_application1/src/main.rs
@@ -1,16 +1,7 @@
-use std::mem;
+fn operators() { 
+    let three_cubed = isize::pow(3, 3);
+    println!("{}", three_cubed);
+}
 fn main() {
-    let a:u8 = 123;
-    println!("the value of a is {}", a);
-    let mut b:i8 = 0;
-    println!("b = {}", b);
-    b = 42;
-    println!("b = {}", b);
-    let mut c = 123456789;
-    println!("c = {}, size = {} bytes", c, mem::size_of_val(&c));
-    c = -1;
-    println!("c = {}", c);
-    let z:isize = 123;
-    let size_of_z = mem::size_of_val(&z);
-    println!("z = {}, takes up {} bytes, {}-bit OS", z, size_of_z, size_of_z * 8);
+    operators();
 }
