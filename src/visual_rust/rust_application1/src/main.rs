c2a7571de82baefcdd7cb8eb0d091216c7a69010
kushal <kushaldeveloper@gmail.com>
2016-06-09 16:56:29 -0400

    learn order of operations

--- src/visual_rust/rust_application1/src/main.rs
+++ src/visual_rust/rust_application1/src/main.rs
@@ -1,6 +1,10 @@
 fn operators() { 
     let three_quad = isize::pow(3, 4);
     println!("{}", three_quad);
+    let oneplusone = 1 + 1;
+    println!("one plus one is {}", oneplusone);
+    let twoplusthreetimesfour = 2 + 3 * 4;
+    println!("2 + 3 * 4 = {}", twoplusthreetimesfour);
 }
 fn main() {
     operators();
